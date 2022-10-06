58b9c85d96df6010015b218b2336ee1e52c70a72
Michael Chinye <michaelchinye2018@gmail.com>
2022-10-06 10:12:04 +0100

    Modified app

--- src/components/checkout/Review.jsx
+++ src/components/checkout/Review.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import {Typography, List, ListItem, ListItemText } from '@mui/material'
-import { isFocusable } from '@testing-library/user-event/dist/utils';
 function Review({checkoutToken}) {
+     function truncate(str, n){
+          return str?.length > n ? str.substr(0, n-1) + "..." : str;
+     }
      if(checkoutToken)
   return (
     <>
-          <Typography variant = 'h6' gutterBottom>Order Summary</Typography>
+          <Typography  variant = 'h6' gutterBottom>Order Summary</Typography>
           <List disablePadding>
                {checkoutToken.line_items.map((product) => (
-                    <ListItem style={{ padding: '10px 0' }} key={product.name}>
-                         <ListItemText primary={product.name} secondary={`Quantity: ${product.quantity}`} />
+                    <ListItem style={{ padding: '10px 0px' }} key={product.name}>
+                         <ListItemText primary={truncate(product.name, 18)} secondary={`Quantity: ${product.quantity}`} />
                          <Typography variant="body2">{product.line_total.formatted_with_symbol}</Typography>
                   </ListItem>
                ))}
-               <ListItem style={{ padding: '10px 0' }}>
+               <ListItem style={{ padding: '10px 0px' }}>
                     <ListItemText primary="Total" />
                     <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
                          {checkoutToken.subtotal.formatted_with_symbol}
